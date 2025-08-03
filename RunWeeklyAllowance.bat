3791fde48959affbc78868f1bf17592bbd5651b6
Justin Fiore <justin.fiore@gmail.com>
2025-08-03 15:05:39 -0400

    Make the scripts run on Java 8

--- RunWeeklyAllowance.bat
+++ RunWeeklyAllowance.bat
@@ -1,4 +1,6 @@
 cd C:\Users\Justin\Documents\workspace\YNABApiUtils\
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_144
+set PATH=C:\Program Files\Java\jdk1.8.0_144\bin;%PATH%
 set YNAB_ACCESS_TOKEN=f29371b2aa962c538e01f0599a7806a921252c262923e74eceab1bf8e83dacdf
 call gradlew.bat --no-daemon installDist
 
